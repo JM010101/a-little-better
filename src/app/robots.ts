f863791195f01940e6daec53dc61e730f1f34943
LeadTool User <rayparker6515@gmail.com>
2025-12-18 10:36:20 -0500

    optimization applied forth-sitemap 2025.12.17 MVP

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
+        allow: ['/', '/sitemap.xml'],
         disallow: [
           '/api/',
           '/_next/',
