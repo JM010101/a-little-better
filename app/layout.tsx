c555aca8ed7ed7e3f0d933ad71a0718b11de93fe
JM010101 <applebig909090@outlook.com>
2026-01-28 16:30:31 -0500

    changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Agency Template",
+  title: "A Little Better",
   description: "We create pleaseant experiences for your customers."
 };
 
